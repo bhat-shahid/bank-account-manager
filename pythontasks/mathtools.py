71557e945c8a7229371a3c7a75cb8c2e0aa3795f
bhat-shahid <127488031+bhat-shahid@users.noreply.github.com>
2025-08-16 01:09:24 +0530

    Delete pythontasks directory

--- pythontasks/mathtools.py
+++
@@ -1,8 +0,0 @@
-def square(n):
-    return n * n
-
-def cube(n):
-    return n ** 3
-
-def is_even(n):
-    return n % 2 == 0
--- pythontasks/string.py
+++
@@ -1,5 +0,0 @@
-string="1, 2, 44, 56, 67"
-strr=string.split(",")
-
-for i in strr:
-    print(type(int(i)))
